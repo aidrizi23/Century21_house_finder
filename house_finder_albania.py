2337370b37ad7adad0e335678d3c8f4e26c65f8b
Albi Idrizi <albiidrizi27@gmail.com>
2024-08-19 01:11:01 +0200

    albania house finder century 21 test

--- house_finder_albania.py
+++ house_finder_albania.py
@@ -6,7 +6,6 @@
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 # import pandas as pd
-##
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
